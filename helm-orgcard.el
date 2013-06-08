d863c577fc3b141a6d786286437116960b06ce67
myuhe <yuhei.maeda@gmail.com>
2013-06-08 20:30:02 +0900

    Fix Commentary

--- helm-orgcard.el
+++ helm-orgcard.el
@@ -28,7 +28,7 @@
 ;; Put the helm-orgcard.el to your
 ;; load-path.
 ;; Add to .emacs:
-;; (require 'anything-gist)
+;; (require 'helm-orgcard)
 ;;
 ;; Original version is anything-orgcard, and port to helm.
 ;; https://gist.github.com/kiwanami/1345100
